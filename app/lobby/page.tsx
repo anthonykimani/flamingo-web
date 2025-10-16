91cb367afb13e153c6d2daa88a874194490fbf00
anthony <kimaniantoni@gmail.com>
2025-10-16 07:40:48 +0300

    feat: wraps components using useSearchParams under Suspense

--- app/lobby/page.tsx
+++ app/lobby/page.tsx
@@ -1,10 +1,12 @@
 import GamePin from '@/components/custom/game_pin'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 const GameLobby = () => {
     return (
         <div className='start-screen-background h-full md:h-screen w-screen bg-no-repeat bg-cover md:flex md:justify-center md:items-center p-1 sm:p-3'>
-            <GamePin />
+            <Suspense>
+                <GamePin />
+            </Suspense>
         </div>
     )
 }
