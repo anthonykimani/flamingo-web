dd9be0e65a304d0e3a387d6276d8fd72e325f702
anthony <kimaniantoni@gmail.com>
2025-10-25 01:33:32 +0300

    feat: start-screen finetuning

--- components/custom/start_screen.tsx
+++ components/custom/start_screen.tsx
@@ -14,12 +14,13 @@ const StartScreen = () => {
 
     return (
         <div className="flex flex-col p-2 gap-2 start-screen-background h-screen bg-no-repeat bg-cover justify-center items-center">
-            <h1 className="font-[Oi] text-white [text-stroke:_2px_black] text-3xl sm:text-8xl ">
+            <h1 className="font-[Oi] text-white [-webkit-text-stroke:2px_black] sm:[-webkit-text-stroke:3px_black] text-4xl xsm:text-6xl sm:text-8xl">
                 Flamingo
             </h1>
-            <div className="flex flex-col sm:flex-row justify-center w-full">
+            <div className="flex flex-col sm:flex-row justify-center w-full gap-2">
                 <Button variant={"active"} className="m-2" size={"xl"} onClick={()=>createGame()} ><MagicWandIcon size={32} />Create a Game</Button>
-                <Button variant={"default"} className="m-2" onClick={()=>router.push("/join")}><GameControllerIcon size={32} />Join a Game</Button>
+                
+                <Button variant={"active"} className="m-2" onClick={()=>router.push("/join")}><GameControllerIcon size={32} />Join a Game</Button>
             </div>
         </div>
     )
